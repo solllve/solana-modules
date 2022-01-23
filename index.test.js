70210d411814cb8135418f118d3c6602fc303043
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-23 10:59:30 -0800

    cleaning

--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
-const uInt8 = require('./index').uInt8
-const testPubKey = require('./index').keypairPubId
-const sum = require('./index').sum
-
-console.log('test ket' + uInt8)
+const app = require('./index')
 
 test('public key is uInt8', () => {
-    expect(typeof uInt8 !== 'array').toBe(true)
+    expect(typeof app.uInt8 !== 'object').toBe(true)
 })

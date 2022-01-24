b08dc21e01de5cbfd05688332754dadcbbdf72e5
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-23 16:35:55 -0800

    cleaning

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const solanaWeb3 = require('@solana/web3.js');
-
 //example wallet. Do not use this wallet for real transactions.
 const keypairPubId = [24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
 const keypairSecret = [164, 117,  35, 213, 180,  22, 209, 252,  68,  15, 207, 248, 165, 203, 200,   1, 145,   4, 224, 225,   4,  80, 192, 101,  13,  59, 124,  63, 101,  22, 137, 126,  24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
-
 module.exports = {
     keypairPubId,
     keypairSecret,
-    uInt8: (arr) => {
-        return Uint8Array.from(arr)
+    uInt8: (obj) => {
+        typeof obj === 'object' ? Uint8Array.from(obj) : console.error('uInt8 expects a key')
     },
-    b64: (arr) => {
-        return Buffer.from(arr).toString('base64');
+    b64: (obj) => {
+        if (typeof obj !== 'object') {
+            console.error('b64 expects a key')
+        }
+        return Buffer.from(obj).toString('base64')
     },
     connection: (env) => {
-        return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl(env));
+        if (typeof env === 'string' && env === 'testnet' || env === 'mainnet' || env === 'devnet') {
+            return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl(env));
+        }
+        return console.error('not a valid environment. Please use testnet, devnet or mainnet')
     },
     generateKeyPair: () => {
         return solanaWeb3.Keypair.generate()
@@ -23,9 +27,9 @@ module.exports = {
         let secret = module.exports.uInt8(arr)
         return solanaWeb3.Keypair.fromSecretKey(secret)
     },
-    transaction: () => {
-        let fromKeypair = module.exports.generateKeyPair();
-        let toKeypair = module.exports.generateKeyPair();
+    transaction: (from, to) => {
+        let fromKeypair = from;
+        let toKeypair = to;
         let transaction = new solanaWeb3.Transaction();
         transaction.add(
             solanaWeb3.SystemProgram.transfer({
@@ -34,6 +38,7 @@ module.exports = {
                 lamports: solanaWeb3.LAMPORTS_PER_SOL
             })
         );
+        return transaction;
     }
 }
 
--- index.test.js
+++ index.test.js
@@ -4,15 +4,16 @@ test('connecting to the solana network', () => {
     expect(app.connection('testnet')).toBeTruthy();
 })
 
-test('uint8 returns object', () => {
-    expect(typeof app.uInt8(app.keypairPubId) === 'object').toBe(true)
-})
+// test('b64 returns string', () => {
+//     expect(typeof app.b64(app.keypairPubId) === 'string').toBe(true)
+// })
 
-test('b64 returns string', () => {
-    expect(typeof app.b64(app.keypairPubId) === 'string').toBe(true)
-})
+// test('b64 returns wallet string', () => {
+//     expect(app.b64(app.keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
+// })
 
-test('b64 returns wallet string', () => {
-    expect(app.b64(app.keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
-})
+console.log(app.transaction(app.keypairPubId, app.keypairPubId))
 
+console.log(typeof app.keypairPubId)
+
+console.log(app.b64(app.keypairPubId))

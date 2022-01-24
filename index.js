94491073428510c9672ef5a21dc41a5a1499b06d
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-23 17:26:16 -0800

    updates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const solanaWeb3 = require('@solana/web3.js');
+const web3 = require('@solana/web3.js');
+const {struct, u32, ns64} = require("@solana/buffer-layout");
+const {Buffer} = require('buffer');
+
 //example wallet. Do not use this wallet for real transactions.
 const keypairPubId = [24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
 const keypairSecret = [164, 117,  35, 213, 180,  22, 209, 252,  68,  15, 207, 248, 165, 203, 200,   1, 145,   4, 224, 225,   4,  80, 192, 101,  13,  59, 124,  63, 101,  22, 137, 126,  24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
+
 module.exports = {
     keypairPubId,
     keypairSecret,
@@ -16,29 +20,73 @@ module.exports = {
     },
     connection: (env) => {
         if (typeof env === 'string' && env === 'testnet' || env === 'mainnet' || env === 'devnet') {
-            return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl(env));
+            return new web3.Connection(web3.clusterApiUrl(env));
         }
         return console.error('not a valid environment. Please use testnet, devnet or mainnet')
     },
-    generateKeyPair: () => {
-        return solanaWeb3.Keypair.generate()
+    connectionSlot: async (env) => {
+        let connection = module.exports.connection(env)
+        let slot = await connection.getSlot();
     },
+    generateKeyPair: () => web3.Keypair.generate(),
     accessWalletFromSecret: (arr) => {
         let secret = module.exports.uInt8(arr)
-        return solanaWeb3.Keypair.fromSecretKey(secret)
+        return web3.Keypair.fromSecretKey(secret)
     },
     transaction: (from, to) => {
         let fromKeypair = from;
         let toKeypair = to;
-        let transaction = new solanaWeb3.Transaction();
+        let transaction = new web3.Transaction();
         transaction.add(
-            solanaWeb3.SystemProgram.transfer({
+            web3.SystemProgram.transfer({
                 fromPubkey: fromKeypair.publicKey,
                 toPubkey: toKeypair.publicKey,
-                lamports: solanaWeb3.LAMPORTS_PER_SOL
+                lamports: web3.LAMPORTS_PER_SOL
             })
         );
         return transaction;
+    },
+    sendAndConfirm: () => web3.sendAndConfirmTransaction(env, transaction, key),
+    sendAndConfirmWithSigner: (signer) => web3.sendAndConfirmTransaction(env, transaction, signer),
+    sendAndConfirmWithSignerAndKey: (signer, key) => web3.sendAndConfirmTransaction(env, transaction, signer, key),
+    transactionFull: async () => {
+        let keypair = web3.Keypair.generate();
+        let payer = web3.Keypair.generate();
+        let connection = new web3.Connection(web3.clusterApiUrl('testnet'));
+
+        let airdropSignature = await connection.requestAirdrop(
+            payer.publicKey,
+            web3.LAMPORTS_PER_SOL,
+        );
+
+        await connection.confirmTransaction(airdropSignature);
+
+        let allocateTransaction = new web3.Transaction({
+            feePayer: payer.publicKey
+        });
+        let keys = [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}];
+        let params = { space: 100 };
+
+        let allocateStruct = {
+        index: 8,
+        layout: struct([
+            u32('instruction'),
+            ns64('space'),
+        ])
+        };
+
+        let data = Buffer.alloc(allocateStruct.layout.span);
+        let layoutFields = Object.assign({instruction: allocateStruct.index}, params);
+        allocateStruct.layout.encode(layoutFields, data);
+
+        allocateTransaction.add(new web3.TransactionInstruction({
+            keys,
+            programId: web3.SystemProgram.programId,
+            data,
+        }));
+
+        await web3.sendAndConfirmTransaction(connection, allocateTransaction, [payer, keypair]);
     }
+    
 }
 
--- index.test.js
+++ index.test.js
@@ -4,16 +4,26 @@ test('connecting to the solana network', () => {
     expect(app.connection('testnet')).toBeTruthy();
 })
 
-// test('b64 returns string', () => {
-//     expect(typeof app.b64(app.keypairPubId) === 'string').toBe(true)
+// test('get slot connection', () => {
+//     expect(app.connectionSlot()).toBe(expect.any(Promise));
 // })
 
-// test('b64 returns wallet string', () => {
-//     expect(app.b64(app.keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
+test('b64 returns string', () => {
+    expect(typeof app.b64(app.keypairPubId) === 'string').toBe(true)
+    console.log(app.b64(app.keypairPubId))
+})
+
+test('b64 returns wallet string', () => {
+    expect(app.b64(app.keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
+    console.log(typeof app.keypairPubId)
+})
+
+test('generate keypair', () => {
+    expect(typeof app.generateKeyPair() === 'object').toBe(true)
+})
+
+// test('generate keypair', () => {
+//     expect(typeof app.generateKeyPair() === 'object').toBe(true)
 // })
 
 console.log(app.transaction(app.keypairPubId, app.keypairPubId))
-
-console.log(typeof app.keypairPubId)
-
-console.log(app.b64(app.keypairPubId))

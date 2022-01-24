672c88654bee6663f90396a9cd09279ceb6e19e8
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-23 17:29:32 -0800

    moved test wallet to jest

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const web3 = require('@solana/web3.js');
 const {struct, u32, ns64} = require("@solana/buffer-layout");
 const {Buffer} = require('buffer');
-
-//example wallet. Do not use this wallet for real transactions.
-const keypairPubId = [24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
-const keypairSecret = [164, 117,  35, 213, 180,  22, 209, 252,  68,  15, 207, 248, 165, 203, 200,   1, 145,   4, 224, 225,   4,  80, 192, 101,  13,  59, 124,  63, 101,  22, 137, 126,  24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
-
 module.exports = {
-    keypairPubId,
-    keypairSecret,
     uInt8: (obj) => {
         typeof obj === 'object' ? Uint8Array.from(obj) : console.error('uInt8 expects a key')
     },
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 const app = require('./index')
 
+//example wallet. Do not use this wallet for real transactions.
+const keypairPubId = [24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
+const keypairSecret = [164, 117,  35, 213, 180,  22, 209, 252,  68,  15, 207, 248, 165, 203, 200,   1, 145,   4, 224, 225,   4,  80, 192, 101,  13,  59, 124,  63, 101,  22, 137, 126,  24, 169, 198, 110, 243, 184,  58, 212, 250, 208, 156, 136, 231, 236, 191,  36, 241,  31,   5, 224, 220, 206,   0, 139, 113,  45, 188,  35,  79, 176,   3,  21];
+
 test('connecting to the solana network', () => {
     expect(app.connection('testnet')).toBeTruthy();
 })
@@ -9,13 +13,13 @@ test('connecting to the solana network', () => {
 // })
 
 test('b64 returns string', () => {
-    expect(typeof app.b64(app.keypairPubId) === 'string').toBe(true)
-    console.log(app.b64(app.keypairPubId))
+    expect(typeof app.b64(keypairPubId) === 'string').toBe(true)
+    console.log(app.b64(keypairPubId))
 })
 
 test('b64 returns wallet string', () => {
-    expect(app.b64(app.keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
-    console.log(typeof app.keypairPubId)
+    expect(app.b64(keypairPubId)).toBe('GKnGbvO4OtT60JyI5+y/JPEfBeDczgCLcS28I0+wAxU=')
+    console.log(typeof keypairPubId)
 })
 
 test('generate keypair', () => {
@@ -26,4 +30,4 @@ test('generate keypair', () => {
 //     expect(typeof app.generateKeyPair() === 'object').toBe(true)
 // })
 
-console.log(app.transaction(app.keypairPubId, app.keypairPubId))
+console.log(app.transaction(keypairPubId, keypairPubId))
